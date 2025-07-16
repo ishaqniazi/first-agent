61d29a251e9286b26041c0f2d86be5fb6ab97955
ishaqniazi <Niazi2k@yahoo.com>
2025-07-16 20:07:28 +0500

    first commit

---
+++ class.py
@@ -0,0 +1,57 @@
+import os
+from agents import Agent, Runner, AsyncOpenAI, RunConfig, function_tool, OpenAIChatCompletionsModel, handoff, RunContextWrapper, HandoffInputData
+import requests
+external_client = AsyncOpenAI(
+    api_key=os.getenv('GEMINI_API_KEY'),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+external_model = OpenAIChatCompletionsModel(
+    model = 'gemini-2.0-flash',
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model= external_model,
+    model_provider=external_client,
+    tracing_disabled=True,
+)
+
+@function_tool
+def data (price: int):
+    """
+ Search for all products from https://dummyjson.com/products that match the given item
+    and are priced less than or equal to the specified price.
+
+    Return the result in one of the following formats:
+
+    If one item is found:
+    I found one {item} that is close to your specified price:
+
+    *   **{Product Title}:** ${Product Price}
+
+    If multiple items are found:
+    I found {N} {item}s under your specified price:
+
+    *   **{Product Title 1}:** ${Product Price 1}
+    *   **{Product Title 2}:** ${Product Price 2}
+    ...
+
+    If no items are found:
+    Sorry, no {item}s found under ${price}.
+"""
+    response = requests.get("https://dummyjson.com/products")
+    products = response.json().get("products", [])
+    return products if products else "No matching products found."
+
+agent = Agent(
+    name="searchData_agent",
+    instructions="you are a helpful assistant that provide search url and return the result from the given tool.",
+    tools=[data])
+
+result = Runner.run_sync(
+    agent,
+    input=input("Enter the price and item to search: "),
+    run_config=config)
+
+print(result.final_output)
